bed68fd6f0808dfcd3fcefb67065f5f54affe528
samkramer6 <samkramer6@vt.edu>
2023-06-19 14:22:03 +0800

    Adding filter_data file

---
+++ Libraries/filter_data.m
@@ -0,0 +1,20 @@
+function filter_data(test_data)
+% 
+%   This is a function that will be used to filter the data that is
+%   collected by the array. This filter will use a 4th order bandpass
+%   butterworth to filter the background noise out of the data.
+%
+%   Sam Kramer
+%   June 19th, 2023
+%
+
+% --Develop filter function
+
+% --Filter data
+
+% --Return data
+
+
+
+end
+

c90346d41bb64f1f8eee7ede49024fedb30e31d1
samkramer6 <samkramer6@vt.edu>
2023-06-19 14:35:45 +0800

    Update to filter_data

--- Libraries/filter_data.m
+++ Libraries/filter_data.m
@@ -1,20 +1,31 @@
-function filter_data(test_data)
+function filtered_data = filter_data(test_data,filter_center,Fs)
 % 
 %   This is a function that will be used to filter the data that is
 %   collected by the array. This filter will use a 4th order bandpass
-%   butterworth to filter the background noise out of the data.
+%   butterworth to filter the background noise out of the data. This will
+%   have a 15% band around the center filter frequency.
+%
+%   Inputs: test_data   --  The raw test data from the daq
+%           filter_center   --  The filter center of bandpass in Hz.
+%           Fs  --  The sample frequency of the data.
+%
+%   Outputs: filtered_data -- The processed filtered data
 %
 %   Sam Kramer
 %   June 19th, 2023
 %
 
-% --Develop filter function
+% --Develop filter band
+    lower_filt = filter_center - 0.15*filter_center;
+    upper_filt = filtere_center + 0.15*filter_center;
+    filter_band = [lower_filt upper_filt] ./ (Fs/2);
+
+% --Define filter used
+    bandpass_filt = butter(4,filter_band);
 
 % --Filter data
 
 % --Return data
-
-
 
 end
 

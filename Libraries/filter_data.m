02973be460e5f4bee9bd128be89e8a393166cbf6
samkramer6 <samkramer6@vt.edu>
2023-06-22 12:15:59 +0800

    Addition of visualize_filtered_data function

--- Libraries/filter_data.m
+++ Libraries/filter_data.m
@@ -1,11 +1,11 @@
 function filtered_data = filter_data(test_data,filter_center,Fs)
 % 
-%   This is a function that will be used to filter the data that is
-%   collected by the array. This filter will use a 4th order bandpass
-%   butterworth to filter the background noise out of the data. This will
-%   have a 15% band around the center filter frequency.
+%   This is a function that will be used to filter the data for one 
+%   microphone that is collected by the array. This filter will use a 4th 
+%   order bandpass butterworth to filter the background noise out of the 
+%   data. This will have a 15% band around the center filter frequency. 
 %
-%   Inputs: test_data   --  The raw test data from the daq
+%   Inputs: test_data   --  The raw test data for one mic
 %           filter_center   --  The filter center of bandpass in Hz.
 %           Fs  --  The sample frequency of the data.
 %
@@ -15,21 +15,16 @@ function filtered_data = filter_data(test_data,filter_center,Fs)
 %   June 19th, 2023
 %
 
-% --Pull out test data
-    data = load(test_data);
-    data = data.final_output_data;
-
 % --Develop filter band
     lower_filt = filter_center - 0.15*filter_center;
     upper_filt = filtere_center + 0.15*filter_center;
     filter_band = [lower_filt upper_filt] ./ (Fs/2);
 
 % --Define filter used
-    bandpass_filt = butter(4,filter_band);
+    [B,A] = butter(4,filter_band);
 
-% --Filter data
-    
-% --Return data
-
+% --Filter data and return
+    filtered_data = filtfilt(B,A,test_data);
+   
 end
 

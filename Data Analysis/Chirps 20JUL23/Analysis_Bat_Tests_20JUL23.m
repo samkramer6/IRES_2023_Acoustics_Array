147406d2986ef698d9bdaea7d8c284d1e66d7362
samkramer6 <samkramer6@vt.edu>
2023-07-26 16:48:19 +0800

    Daily Work (26JUL23)

--- Data Analysis/Chirps 20JUL23/Analysis_Bat_Tests_20JUL23.m
+++ Data Analysis/Chirps 20JUL23/Analysis_Bat_Tests_20JUL23.m
@@ -29,19 +29,64 @@
 % --Another Isolated
     spectrogram_data(data_path,32,2.428,2.438)
 
-% --Pulling out the data to create a "Model Chirp" from first chirp
-    load(data_path)
-    final_output_data = final_output_data(2:end-1,:);
-    mic_data = final_output_data(:,2:width(final_output_data));
-    time = final_output_data(:,1);
-    fs = round(1/(time(10)-time(9)));
+% --Double Isolated Chirps
+    spectrogram_data(data_path,32,0.4,0.44)
 
-% --Model Chirp
-    model_chirp = mic_data(2.951*fs:2.96*fs,32);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Mic 26 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+% --First chirp
+    spectrogram_data(data_path,mic_number,3.12,3.14)
 
-% --Filter out the model chirp
-    filtered_model_chirp = filter_data(model_chirp,fs,120000,0.2,"false");
+% --Second chirp
+    spectrogram_data(data_path,mic_number,3.02,3.03)
 
+% --Third Chirp
+    spectrogram_data(data_path,mic_number,3.5,3.51)
+
+% --Fourth Chirp
+    spectrogram_data(data_path,mic_number,2.83,2.84)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Mic 28 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% --First Chirp
+    
+    
+%% Running Correlations to find chirps
+% --Loading in new Dataset
+    mic_number = 28;
+    mic = mic_data(:,mic_number);
+    mic = filter_data(mic,fs,120000,0.2,"false");
+
+% --Cross correlate the dataset
+    [correlated_data,lags] = xcorr(mic,filtered_model_chirp);
+    time = (1/fs).*(-length(mic)+1:length(mic)-1);
+
+% --Plot
+    plot(time,correlated_data)
+        xlim([0 max(time)-(1/fs)])
+        xlabel("Approximate Test Time (s)")
+        ylabel("Correlation")
+        title("Correlation of mic 26 to the chirps")
+        grid on
+
+%{
+    There appears to be a large correlation spike at the middle of the
+    data set. This lag would correspond to a specific position within the
+    dataset correlating to position by n*fs - 1/fs, so this would give us a
+    position at roughly 2.951 seconds which is where the other spike was in
+    the dataset in the first test. Lets look at the spectrogram now.
+%}
+
+% --Spectrogram data
+    spectrogram_data(data_path,mic_number,2.94,2.96)
+
+% --Plot out that section of the data
+    %micmic = mic
+    figure()
+    plot(mic(fs*2.948:fs*2.96))
+    
+
+%% Creating a Model Chirp for Correlation data
 %{
     This filtered model chirp is located within the libraries
     folder and is mainly used for running correlations versus the
@@ -52,5 +97,11 @@
     July 26th, 2023
 %}
 
+% --Pulling out the data to create a "Model Chirp" from first chirp
+    [mic_data,~,fs] = load_data(data_path);
 
-% --Model
+% --Model Chirp
+    model_chirp = mic_data(2.951*fs:2.96*fs,32);
+
+% --Filter out the model chirp
+    filtered_model_chirp = filter_data(model_chirp,fs,120000,0.2,"false");

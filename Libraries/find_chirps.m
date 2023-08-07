3062e10bfa77166ef2891699feb56e09287a77b7
samkramer6 <samkramer6@vt.edu>
2023-08-07 15:43:34 +0800

    Resolving branch issues

--- Libraries/find_chirps.m
+++ Libraries/find_chirps.m
@@ -17,7 +17,7 @@ function find_chirps(data_path,mic_num,bat_type)
     clc;
 
 % --Load in data
-    [data,time,fs] = load_data(data_path);
+    [data,~,fs] = load_data(data_path);
     disp("Loaded data successfully.")
 
 % --Select Microphone out of data
@@ -28,7 +28,6 @@ function find_chirps(data_path,mic_num,bat_type)
     bat_type = string(bat_type);
     bat_type = char(upper(bat_type));
     try
-<<<<<<< HEAD
         if bat_type(1) == 'H'
             % --Load in hippo call
                 hippo_call = load("Hippo_example_chirp.mat");
@@ -40,12 +39,6 @@ function find_chirps(data_path,mic_num,bat_type)
 
         elseif bat_type(1) == 'R'
             % --Load in rhino calls
-                
-=======
-        if bat_type == "Hipposideros" || bat_type == "hipposideros"
-            hippo_call = load("Hippo_example_chirp.mat");
-            hippo_call = hipp_call.model_chirp;
->>>>>>> 98870ca84eec167b39efa494ca58e30d064f4946
         end
     catch
         % --Error handler
@@ -64,11 +57,7 @@ function find_chirps(data_path,mic_num,bat_type)
 
 % Ghost Code:
     % --Compare the two time domain signals {Call time_domain_finder()}
-<<<<<<< HEAD
         time_indeces = time_domain_finder(data,fs,FM_chirp,CFFM_chirp);
-=======
-        indeces = time_domain_finder(data,fs,FM_chirp,hippo_call);
->>>>>>> 98870ca84eec167b39efa494ca58e30d064f4946
         
     % --Outline for time domain section
         fprintf("Time Domain Finder Done.\n")
--- Libraries/time_domain_finder.m
+++ Libraries/time_domain_finder.m
@@ -18,48 +18,24 @@ function time_indeces = time_domain_finder(data,fs,FM_chirp,CFFM_chirp)
             disp("Ensure that Nyquist is above 200kHz")
     end
 
-<<<<<<< HEAD
+
 % --Cross correlate vs. simple chirps
     [FM_corr, ~] = xcorr(filtered_data, FM_chirp);
     [CFFM_corr, ~] = xcorr(filtered_data, CFFM_chirp);
-=======
-% --Cross correlate vs. simple FM chirp
-    [FM_corr, FM_lags] = xcorr(filtered_data, FM_chirp);
-
-% --Cross correlate vs. example CFFM chirp
-    [CFFM_corr, CFFM_lags] = xcorr(filtered_data, CFFM_chirp);
->>>>>>> 98870ca84eec167b39efa494ca58e30d064f4946
 
 % --Reformat data to find just the single sided cross correlation
     CFFM_corr = CFFM_corr(length(CFFM_corr)/2:length(CFFM_corr));
     FM_corr = FM_corr(length(FM_corr)/2:length(FM_corr));
 
 % --Develop peak threshold criteria
-<<<<<<< HEAD
     CFFM_corr = abs(CFFM_corr);
     FM_corr = abs(FM_corr);
     CFFM_peak_threshold = max(CFFM_corr) - 1.5*mean(CFFM_corr);
     FM_peak_threshold = max(FM_corr) - 1*mean(FM_corr);
     
 % --Find peaks of both correlations
-    [~,CF_ind] = findpeaks(CFFM_corr,"MinPeakHeight",CFFM_peak_threshold);     % ADD MIN PEAK HEIGHT
-    [~,FM_ind] = findpeaks(FM_corr,"MinPeakHeight",FM_peak_threshold);       % ADD MIN PEAK HEIGHT
-=======
-    CF_thresh = mean(abs(CFFM_corr));
-    FM_thresh = mean(abs(FM_corr));
-    
-% --Find peaks of both correlations
-    [CF_peaks,CF_ind] = findpeaks(CFFM_corr,'MinPeakHeight',CF_thresh);     
-    [FM_peaks,FM_ind] = findpeaks(FM_corr,'MinPeakHeight',FM_thresh);
-
-    % --Convert index to time [DELETE AFTER DEVELOPMENT]
-        CF_time = CFFM_lags./fs;
-        FM_time = FM_lags./fs;
-
-    % --Plot Both [DELETE AFTER DEVELOPMENT OF FUNCTION]
-        plot(CF_time,CF_peaks);
-        plot(FM_time,FM_peaks);
->>>>>>> 98870ca84eec167b39efa494ca58e30d064f4946
+    [~,CF_ind] = findpeaks(CFFM_corr,"MinPeakHeight",CFFM_peak_threshold);
+    [~,FM_ind] = findpeaks(FM_corr,"MinPeakHeight",FM_peak_threshold);
 
 % --Compare the peaks of both {Calls Comparison Function}
     time_indeces = compare_function(CF_ind./fs, FM_ind./fs, 0.04);
